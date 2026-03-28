9d00fd43afba9aca2741cc83af965e985c258c87
sausageOps <amegomaster@gmail.com>
2026-03-28 12:52:25 +0530

    Refactored game with proper stracture and features

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import random
 
-number = random.randint(1, 100)
-
-print("🎮 Welcome to Number Guessing Game")
+print("=" * 30)
+print("🎮 Number Guessing Game")
+print("=" * 30)
 
 while True:
-    try:
-        guess = int(input("Enter your guess (1-100): "))
+    choice = input("Choose difficulty (easy / medium / hard): ").lower()
 
-        if guess == number:
-            print("🎉 You guessed it right!")
-            break
-        elif guess < number:
-            print("Too low 🔻")
-        else:
-            print("Too high 🔺")
+    if choice == "easy":
+        max_num = 50
+    elif choice == "medium":
+        max_num = 100
+    else:
+        max_num = 200
 
-    except ValueError:
-        print("Please enter a valid number!")
+    number = random.randint(1, max_num)
+    max_attempts = 7
+    attempts = 0
+
+    while attempts < max_attempts:
+        try:
+            guess = int(input(f"Enter your guess (1-{max_num}): "))
+            attempts += 1
+
+            if guess == number:
+                print(f"🎉 You guessed it in {attempts} attempts!")
+                break
+
+            elif guess < number:
+                print("Too low 🔻")
+            else:
+                print("Too high 🔺")
+
+            if abs(number - guess) <= 5:
+                print("🔥 Very close!")
+
+        except ValueError:
+            print("Please enter a valid number!")
+
+    else:
+        print(f"💀 Game Over! The number was {number}")
+
+    again = input("Play again? (y/n): ")
+    if again != "y":
+        print("Thanks for playing!")
+        break
